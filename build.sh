d2149394d0ec29ff6896e0d2aa9c1ac7d0fd5b67
Soichi Hayashi <hayashis@iu.edu>
2017-05-24 23:44:34 +0000

    created subdirectories for each platform/matlab version

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-docker build -t brainlife/mcr .
-docker tag brainlife/mcr brainlife/mcr:R2016a
-docker push brainlife/mcr:R2016a
+
+#docker build -t brainlife/mcr .
+#docker tag brainlife/mcr brainlife/mcr:R2017a
+#docker push brainlife/mcr:R2017a
+
+#docker build -t brainlife/mcr:centos6-r2016a centos6-r2016a && docker push brainlife/mcr:centos6-r2016a
+
+docker build -t brainlife/mcr:centos6-r2017a centos6-r2017a && docker push brainlife/mcr:centos6-r2017a
