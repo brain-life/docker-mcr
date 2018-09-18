fba03acee7b79263694c0a16f9bef20c6c5da097
Soichi Hayashi <hayashis@iu.edu>
2018-09-18 19:14:15 +0000

    added 2012b (used by freesurfer)

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 #docker pull centos:7
 #docker pull neurodebian:xenial
 
+#docker pull neurodebian:trusty
+
 #docker build -t brainlife/mcr .
 #docker tag brainlife/mcr brainlife/mcr:R2017a
 #docker push brainlife/mcr:R2017a
@@ -17,5 +19,6 @@
 #docker build -t brainlife/mcr:centos7-r2017a centos7-r2017a && 
 #    docker push brainlife/mcr:centos7-r2017a
 
-docker build -t brainlife/mcr:neurodebian1604-r2017a neurodebian1604-r2017a && \
-    docker push brainlife/mcr
+#docker build -t brainlife/mcr:neurodebian1604-r2017a neurodebian1604-r2017a && docker push brainlife/mcr
+
+docker build -t brainlife/mcr:neurodebian1604-r2012b neurodebian1604-r2012b && docker push brainlife/mcr
